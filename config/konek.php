825f296bc201ffdd913db765e3cd90c09a0c9198
Muhammad iqbalk <90741174+MuhammadIqbalK@users.noreply.github.com>
2024-11-05 01:55:53 +0700

    Update konek.php

--- config/konek.php
+++ config/konek.php
@@ -1,8 +1,8 @@
 <?php
-$host="10.62.175.21";
-$user="develop";
-$pass="dev2019";
-$port="5433";
-$dbname="develop";
+$host="########";
+$user="########";
+$pass="########";
+$port="####";
+$dbname="####";
 $conn=pg_connect("host=".$host." port=".$port." dbname=".$dbname." user=".$user." password=".$pass);
 ?>
